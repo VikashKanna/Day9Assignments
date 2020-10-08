5c450d29b47ae1bec06996e1fbf6848718029218
Vikash Kanna <Enter Git Registered Email id>
2020-10-09 01:33:34 +0530

    Calculate Wages for a Month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -24,17 +24,24 @@ main
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+numWorkingDays=20;
 
-case $randomCheck in
-        $isFullTime)
-	            empHrs=8;
-                       ;;
-        $isPartTime)
-                    empHrs=4;
-                       ;;
-               *)
-                    empHrs=0;
-esac
-salary=$(($empRatePerHr*$empHrs));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(($RANDOM%3));
+		case $empCheck in
+			$isFullTime)
+				empHrs=8
+				;;
+			$isPartTime)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+		salary=$(($empHrs*$empRatePerHr));
+		totalSalary=$(($totalSalary+$salary));
+done
