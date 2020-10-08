f35ab98088f295813f1173855011caaba008e16a
Vikash Kanna <Enter Git Registered Email id>
2020-10-09 02:06:40 +0530

    Store the Daily wage along with the total wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -24,32 +24,34 @@ main
 
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTHS=100;
+MAX_HRS_IN_MONTH=100;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-totalWorkHours=0;
+totalEmpHr=0;
 totalWorkingDays=0;
 
-function getWorkingHours() {
+function getWorkHrs() {
 	case $1 in
 		$IS_FULL_TIME)
-			workHours=8
+			empHrs=8
 			;;
 		$IS_PART_TIME)
-			workHours=4
+			empHrs=4
 			;;
 		*)
-			workHours=0
+			empHrs=0
 			;;
 	esac
 }
 
-while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTHS && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTHS && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	getWorkingHours $((RANDOM%3))
-	totalWorkHours=$(($totalWorkHours + $workHours))
+	empCheck=$((RANDOM%3))
+	getWorkHrs $empCheck
+	totalEmpHr=$(($totalEmpHr+$empHrs))
+	dailyWages[$totalWorkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
 done
 
-totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
+totalSalary="$( getEmpWage $totalEmpHr )"
